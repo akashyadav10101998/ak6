e087a9fc70f950ca4418b039cd71f75454a4fdcc
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-10-03 16:49:09 +0530

    Add files via upload

---
+++ ak6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int addition (int a,int b)
+{
+int z;
+z = a + b;
+return (z);
+}
+
+int main()
+{
+ int x;
+ x= addition (2,4);
+ cout << "the result is" << x;
+return 0;
+}
